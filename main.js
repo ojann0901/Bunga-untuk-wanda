3014e1c46b2c3534f3aa841d3fc6930b673d81ec
ojann0901 <fauzanikhsan36@gmail.com>
2026-06-21 07:56:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+// Memutar musik saat layar diklik/disentuh
+document.body.addEventListener('click', function() {
+    const audio = document.getElementById('musik');
+    const hint = document.getElementById('hint-teks');
+    
+    if (audio.paused) {
+        audio.play();
+        hint.style.display = 'none'; // Menyembunyikan teks petunjuk setelah lagu diputar
+    }
+});
